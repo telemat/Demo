560898becb1d42f1c74a70c05de42e16c97d2332
Kalil <khalil.dowlut@gmail.com>
2015-09-21 21:39:17 +0400

    Work in progress.

--- FlickrApp/FlickrApp/FlickrApp.WindowsPhone/ViewModels/MainPageViewModel.cs
+++ FlickrApp/FlickrApp/FlickrApp.WindowsPhone/ViewModels/MainPageViewModel.cs
@@ -2,6 +2,9 @@
 {
     #region Imports
 
+    using System;
+    using System.Windows.Input;
+    using Common;
     using PropertyChanged;
 
     #endregion
@@ -9,15 +12,24 @@
     [ImplementPropertyChanged]
     public class MainPageViewModel
     {
+        private readonly Lazy<ICommand> _cmdAuthenticate;
+
         public MainPageViewModel()
         {
-            AuthenticationKey = "Hello";
+            
 
-            SecretCode = "World";
-        }
+            _cmdAuthenticate = new Lazy<ICommand>(() => new RelayCommand(DoIt));
+        }        
 
         public string AuthenticationKey { get; set; }
 
         public string SecretCode { get; set; }
+
+        public ICommand AuthenticateCommand => _cmdAuthenticate.Value;
+
+        private void DoIt()
+        {
+            SecretCode = "Hello there!!!";
+        }
     }
 }
