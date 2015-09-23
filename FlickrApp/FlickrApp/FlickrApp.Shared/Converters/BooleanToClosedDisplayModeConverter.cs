7457206ba91002bc5b3fedfb39568f0d7ce0b90c
Kalil <khalil.dowlut@gmail.com>
2015-09-24 00:47:48 +0400

    Added.

---
+++ FlickrApp/FlickrApp/FlickrApp.Shared/Converters/BooleanToClosedDisplayModeConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace FlickrApp.Converters
+{
+    #region Imports
+
+    using System;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Data;
+
+    #endregion
+
+    public class BooleanToClosedDisplayModeConverter
+        : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var isMinimal = (bool) value;
+            return isMinimal ? AppBarClosedDisplayMode.Minimal : AppBarClosedDisplayMode.Compact;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (AppBarClosedDisplayMode) value == AppBarClosedDisplayMode.Minimal;
+        }
+    }
+}
