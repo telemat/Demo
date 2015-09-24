76fa589b03fea8344e9eb9d4095cae77130ecb7f
Kalil <khalil.dowlut@gmail.com>
2015-09-24 11:02:35 +0400

    Added support for tablet.

--- FlickrApp/FlickrApp/FlickrApp.Shared/Converters/BooleanToClosedDisplayModeConverter.cs
+++ FlickrApp/FlickrApp/FlickrApp.Shared/Converters/BooleanToClosedDisplayModeConverter.cs
@@ -14,12 +14,20 @@
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var isMinimal = (bool) value;
+#if WINDOWS_PHONE_APP
             return isMinimal ? AppBarClosedDisplayMode.Minimal : AppBarClosedDisplayMode.Compact;
+#else
+            return null;
+#endif
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
+#if WINDOWS_PHONE_APP
             return (AppBarClosedDisplayMode) value == AppBarClosedDisplayMode.Minimal;
+#else
+            return null;
+#endif
         }
     }
 }
