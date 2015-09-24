5b6c6297606c6a82180bd1ec27efd2b84707e9b4
Kalil <khalil.dowlut@gmail.com>
2015-09-24 22:17:48 +0400

    Moved.

---
+++ FlickrApp/FlickrApp/FlickrApp.Shared/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace FlickrApp
+{
+    #region Imports
+
+    using Autofac;
+    using Core;
+    using ViewModels;
+
+    #endregion
+
+    internal sealed class Startup
+    {
+        public void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new CoreModule());
+
+            // viewmodels
+            builder.RegisterType<SearchPageViewModel>().AsSelf().SingleInstance();
+            builder.RegisterType<PivotPageViewModel>().AsSelf().SingleInstance();
+            builder.RegisterType<LocationPageViewModel>().AsSelf().SingleInstance();
+
+            var container = builder.Build();
+
+            Resolver.Instance.RegisterContainer(container);
+        }
+    }
+}
