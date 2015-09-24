d3fb41b6ffd331c4145fe5501761bd0dba835ae3
Kalil <khalil.dowlut@gmail.com>
2015-09-24 11:03:40 +0400

    Added timestamp.

--- FlickrApp/FlickrApp.Contracts/Models/GeoLocation.cs
+++ FlickrApp/FlickrApp.Contracts/Models/GeoLocation.cs
@@ -1,7 +1,11 @@
 ﻿namespace FlickrApp.Contracts.Models
 {
+    using System;
+
     public class GeoLocation
     {
+        public DateTime Timestamp { get; set; }
+
         public double Latitude { get; set; }
 
         public double Longtitude { get; set; }
